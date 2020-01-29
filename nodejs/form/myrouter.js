f6d2f25c355c2a3e92074e4233bd23b11d2d67ce
dg8456 <dg73@sina.com>
2020-01-29 23:31:09 +0800

    express 2nd

---
+++ nodejs/form/myrouter.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(function(req,res,next) {
+    var log = '用户IP地址：' + req.connection.remoteAddress;
+    console.log(log);
+    next();
+});
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/www/index.html');
+    console.log(__dirname + '/www/index.html');
+});
+
+app.post('/regist', function(req,res){
+    var user= req.body.user;
+    var email=req.body.email;
+    var boards = req.body.boards;
+
+    var html = '昵称：' + user + '<br>' +
+               '电邮：' + email + '<br>' +
+               '特长：' + boards.toString();
+    res.send(html);
+});
+
+app.get('*', function(req,res) {
+    res.status(404);
+    res.send('网页去哪了 :( ');
+});
+
+app.listen(5455, function(req,res){
+    console.log('web post service on working');
+});
---
+++ nodejs/form/post.js
@@ -0,0 +1,30 @@
+﻿var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/www/index.html')
+});
+
+app.post('/regist',function(req,res){
+  var user=req.body.user;
+  var email=req.body.email;
+  var boards = req.body.boards;
+
+  var html = '暱称：' + user + '<br>' +
+             '电邮：' + email + '<br>' +
+             '控制板：' + boards.toString();
+
+  res.send(html);
+});
+
+app.get('*', function(req, res) {
+  res.status(404);
+  res.send('找不到网页！');
+});
+
+app.listen(5438, function(req, res) {
+  console.log("网站服务器在5438端口开工了！");
+});
